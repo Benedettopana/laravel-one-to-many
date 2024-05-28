f2fe65ab4ddb0bab82872521acf47562bc0b695c
Benedettopana <benedetto.pana@gmail.com>
2024-05-28 15:06:27 +0200

    [add]Technology

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Technology;
 use Illuminate\Http\Request;
+use App\Functions\Helper as Help;
+use App\Http\Requests\TechnologyRequest;
 
 class TechnologyController extends Controller
 {
@@ -22,15 +24,29 @@ class TechnologyController extends Controller
      */
     public function create()
     {
-
+        return view('admin.technologies.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TechnologyRequest $request)
     {
-        //
+        $form_data = $request->all();
+        $exist = Technology::where('name', $form_data['name'])->first();
+        if($exist){
+            return redirect()->route('admin.technology.create')->with('error', 'Nome del tipo già esiste');
+        } else{
+            $new_technology = new Technology();
+            $form_data['slug'] = Help::createSlug($form_data['name'], Technology::class);
+
+            //? Riempio e salvo
+            $new_technology->fill($form_data);
+            $new_technology->save();
+
+            //? Ridireziono
+            return redirect()->route('admin.technology.index')->with('success', 'Tecnologia aggiunta correttamente!');
+        }
     }
 
     /**
@@ -44,24 +60,34 @@ class TechnologyController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Technology $technology)
     {
-        //
+        return view('admin.technologies.edit', compact('technology'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TechnologyRequest $request, Technology $technology)
     {
-        //
+        $form_data = $request->all();
+
+        if($form_data['name'] === $technology->name){
+            $form_data['slug'] = $technology->slug;
+            }else{
+                $form_data['slug'] = Help::createSlug($form_data['name'], Technology::class) ;
+            }
+
+            $technology->update($form_data);
+            return redirect()->route('admin.technology.index',$technology);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Technology $technology)
     {
-        //
+        $technology->delete();
+        return redirect()->route('admin.technology.index')->with('deleted', 'La tecnologie'. ' ' . $technology->name. ' ' .'è stata cancellata con successo!');
     }
 }
--- resources/views/admin/technologies/edit.blade.php
+++ resources/views/admin/technologies/edit.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container-xl ">
-    <h1 class="my-5">Crea una nuova Tecnologia</h1>
+    <h1 class="my-5">Modifica una Tecnologia</h1>
     {{-- messaggio di cancellazione avvenuta del progetto --}}
     @if(session('error'))
     <div class="alert alert-danger" role="alert">
     {{session('error')}}
     </div>
     @endif
-    <form action="{{ route('admin.technology.store') }}" method="POST">
+    <form action="{{ route('admin.technology.update', $technology) }}" method="POST">
     @csrf
-
+    @method('PUT')
     <div class="row row-cols-2">
       <div class="col mt-2 ">
         <label for="name">Nome</label>
@@ -20,7 +20,7 @@
           form-control @error('name') is-invalid @enderror"
           id="name"
           name="name"
-          value="{{ old('name')}}"
+          value="{{ $technology->name }}"
         >
         @error('name')
         <small class="text-danger">
@@ -29,10 +29,11 @@
         @enderror
       </div>
 
+        {{-- <textarea type="textarea" class="form-control" id="desc" name="desc"></textarea> --}}
       </div>
     </div>
-    <div class="mt-3">
-        <button type="submit" class="btn btn-outline-success me-3">Crea</button>
+    <div class="">
+        <button type="submit" class="btn btn-outline-success me-3">modifica</button>
         <button type="reset" class="btn btn-outline-danger">Cancella</button>
     </div>
   </form>
